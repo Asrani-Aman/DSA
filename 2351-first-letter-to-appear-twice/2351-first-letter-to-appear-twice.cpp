51ace1c01a7a2d63e982d9f63ceab5cf823eaedd
AMAN ASRANI <96644946+Asrani-Aman@users.noreply.github.com>
2023-07-14 00:46:40 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (14.10%) - LeetHub

---
+++ 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    char repeatedCharacter(string s) {
+        vector<int> hash(256, 0);  // Initialize the hash table with 256 elements
+
+        for (int i = 0; i < s.size(); i++) {
+
+            hash[s[i]]++;
+            if (hash[s[i]] == 2) {
+                return s[i];
+            }
+        }
+
+       
+
+        return '\0'; 
+    }
+};
