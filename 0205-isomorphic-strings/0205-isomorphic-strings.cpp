42026a9a9794b1f6c6dd9528d6934aa9985f86ae
NIKITA ASRANI <96644946+nikki-asrani@users.noreply.github.com>
2023-08-23 23:08:14 +0530

    Time: 8 ms (47.82%), Space: 7 MB (64.26%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        int n = s.size();
+        if(s.size()!= t.size()) return false;
+        map<char, char> mpp;
+
+        for(int i=0; i<n; i++)
+        {
+            if(mpp.find(s[i]) != mpp.end())
+            {
+                if(mpp[s[i]]==t[i]) continue;
+                else return false;
+            }
+            // else mpp[s[i]] = t[i];
+            // else
+            {
+                for(auto it:mpp)
+                {
+                    if(it.second == t[i]) return false;
+                    else continue;
+                }
+                mpp[s[i]] = t[i];
+            }
+        }
+        return true;       
+    }
+};
