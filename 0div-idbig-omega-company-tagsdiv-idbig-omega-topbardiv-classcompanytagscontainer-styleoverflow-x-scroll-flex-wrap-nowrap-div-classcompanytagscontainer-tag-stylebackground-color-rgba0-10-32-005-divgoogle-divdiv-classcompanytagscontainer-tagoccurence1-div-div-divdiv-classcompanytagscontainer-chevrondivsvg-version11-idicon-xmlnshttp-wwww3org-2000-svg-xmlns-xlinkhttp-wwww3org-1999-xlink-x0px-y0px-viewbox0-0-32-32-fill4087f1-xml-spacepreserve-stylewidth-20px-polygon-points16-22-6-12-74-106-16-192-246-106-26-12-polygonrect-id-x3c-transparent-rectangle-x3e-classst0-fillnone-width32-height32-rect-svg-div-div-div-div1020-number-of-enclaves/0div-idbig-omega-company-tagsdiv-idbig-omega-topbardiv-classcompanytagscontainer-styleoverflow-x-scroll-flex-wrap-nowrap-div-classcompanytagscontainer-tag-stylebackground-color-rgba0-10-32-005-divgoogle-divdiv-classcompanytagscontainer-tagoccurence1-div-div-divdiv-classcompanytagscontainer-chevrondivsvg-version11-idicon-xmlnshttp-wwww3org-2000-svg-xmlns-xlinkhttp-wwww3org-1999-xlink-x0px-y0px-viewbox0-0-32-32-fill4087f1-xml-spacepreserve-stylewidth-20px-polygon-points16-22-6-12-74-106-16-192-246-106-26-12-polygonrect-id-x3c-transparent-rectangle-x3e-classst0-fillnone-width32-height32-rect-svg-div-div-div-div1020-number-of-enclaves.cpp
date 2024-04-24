b8a0559c849f968774b79509818e485a32dcd5bd
AMAN ASRANI <96644946+Asrani-Aman@users.noreply.github.com>
2024-04-25 00:51:09 +0530

    Time: 46 ms (83.01%), Space: 30.2 MB (71.44%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence1-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1020-number-of-enclaves/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence1-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1020-number-of-enclaves.cpp
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence1-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1020-number-of-enclaves/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence1-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1020-number-of-enclaves.cpp
@@ -1,108 +1,18 @@
-// class Solution {
-// private:
-//     void dfs(vector<vector<int>>& grid, vector<vector<int>>& vis, int& count,int row,int col, int drow[],int dcol[] )
-//     {
-//         int rows = grid.size();
-//         int cols = grid[0].size();
-//         for(int i=0; i<4; i++)
-//         {
-//             int nrow = row + drow[i];
-//             int ncol = col + dcol[i];
-//             if(vis[nrow][ncol]!=1 && grid[nrow][ncol]==1 && nrow>=0 && nrow<rows && ncol>=0 && ncol<cols )
-//             {
-//                 vis[nrow][ncol]=1;
-//                 count--;
-//                 dfs(grid,vis,count,nrow,ncol,drow,dcol);      
-//             }
-//         }
-//     }
-// public:
-//     int numEnclaves(vector<vector<int>>& grid) {
-//         int rows = grid.size();
-//         int cols = grid[0].size();
-//         vector<vector<int>> vis(rows, vector<int>(cols,0));
-//         int drow[] = {-1,0,+1,0};
-//         int dcol[]= {0,+1,0,-1};
-//         int count =0;
-//         // here i count number of 1's
-//         for(int i =0; i<rows; i++)
-//         {
-//             for(int j=0; j<cols; j++)
-//             {
-//                 if(grid[i][j]==1)
-//                 {
-//                     count++;
-//                 }
-//             }
-//         }
-//         for(int row = 0; row <rows; row++)
-//         {
-//             // first col
-//             if(grid[row][0]==1)
-//             {
-//                 if(vis[row][0] != 1)
-//                 {
-//                     vis[row][0] = 1;
-//                     count--;
-//                     dfs(grid,vis,count,row,0,drow,dcol);
-//                 }
-                
-                
-//             }
-//             // last col
-//             if(grid[row][cols-1]==1)
-//             {
-//                 if(vis[row][cols-1] != 1)
-//                 {
-//                     vis[row][cols-1] = 1;
-//                     count--;
-//                     dfs(grid,vis,count,row,cols-1,drow,dcol);
-//                 }
-                  
-//             }
-//         }
-//         for(int col =0; col<cols; col++)
-//         {
-//             //first row
-//             if(grid[0][col]==1)
-//             {
-//                 if(vis[0][col]!=1)
-//                 {
-//                     vis[0][col]=1;
-//                     count--;
-//                     dfs(grid,vis,count,0,col,drow,dcol);
-//                 }
-//             }
-//             if(grid[rows-1][col]==1)
-//             {
-//                 if(vis[rows-1][col]!=1)
-//                 {
-//                     vis[rows-1][col]=1;
-//                     count--;
-//                     dfs(grid,vis,count,rows-1,col,drow,dcol);
-//                 }
-//             }
-//         }
-//         return count;
-//     }
-// };
-
-
 
 class Solution {
 public:
-     void dfs(std::vector<std::vector<int>>& grid, std::vector<std::vector<int>>& vis, int& count, int row, int col, int drow[], int dcol[]) {
-        vis[row][col] = 1;
-        int rows = grid.size();
-        int cols = grid[0].size();
-        for (int i = 0; i < 4; i++) {
-            int nrow = row + drow[i];
-            int ncol = col + dcol[i];
-            if (nrow >= 0 && nrow < rows && ncol >= 0 && ncol < cols && vis[nrow][ncol] != 1 && grid[nrow][ncol] == 1) {
-                dfs(grid, vis, count, nrow, ncol, drow, dcol);
-            }
-        }
-    }
+    //  void bfs(std::vector<std::vector<int>>& grid, std::vector<std::vector<int>>& vis, int& count, int row, int col, int drow[], int dcol[]) {
+    //     vis[row][col] = 1;
+    //     int rows = grid.size();
+    //     int cols = grid[0].size();
+    //     for (int i = 0; i < 4; i++) {
+    //         int nrow = row + drow[i];
+    //         int ncol = col + dcol[i];
+    //         if (nrow >= 0 && nrow < rows && ncol >= 0 && ncol < cols && vis[nrow][ncol] != 1 && grid[nrow][ncol] == 1) {
+    //             dfs(grid, vis, count, nrow, ncol, drow, dcol);
+    //         }
+    //     }
+    // }
 
     int numEnclaves(vector<vector<int>>& grid) {
         int rows = grid.size();
@@ -111,29 +21,22 @@ public:
         int drow[] = {-1,0,+1,0};
         int dcol[]= {0,+1,0,-1};
         int count =0;
-        // here i count number of 1's
-        // for(int i =0; i<rows; i++)
-        // {
-        //     for(int j=0; j<cols; j++)
-        //     {
-        //         if(grid[i][j]==1)
-        //         {
-        //             count++;
-        //         }
-        //     }
-        // }
+        queue<pair<int,int>> q;
+       
         for(int row = 0; row <rows; row++)
         {
             // first col
             if(grid[row][0]==1 && vis[row][0] != 1)
                 {
-                    dfs(grid,vis,count,row,0,drow,dcol);
+                    vis[row][0] = 1;
+                    q.push({row,0});
                 }
             // last col
             if(grid[row][cols-1]==1 && vis[row][cols-1] != 1)
                 {
                   
-                    dfs(grid,vis,count,row,cols-1,drow,dcol);
+                    vis[row][cols-1] = 1;
+                    q.push({row,cols-1});
                 }
         }
         for(int col =0; col<cols; col++)
@@ -142,15 +45,36 @@ public:
             if(grid[0][col]==1 && vis[0][col]!=1)
                 {
                     
-                    dfs(grid,vis,count,0,col,drow,dcol);
+                    vis[0][col] = 1;
+                    q.push({0,col});
                 }
             
             if(grid[rows-1][col]==1 && vis[rows-1][col]!=1)
                 {
                    
-                    dfs(grid,vis,count,rows-1,col,drow,dcol);
+                    vis[rows-1][col] = 1;
+                    q.push({rows-1,col});
                 }
           
+            
+        }
+        while(!q.empty())
+        {
+            int r = q.front().first;
+            int c = q.front().second;
+            q.pop();
+            for (int i = 0; i < 4; i++)
+            {
+                int nrow = r + drow[i];
+                int ncol = c + dcol[i];
+                if (nrow >= 0 && nrow < rows && ncol >= 0 && ncol < cols && vis[nrow][ncol] != 1 && grid[nrow][ncol] == 1)
+                {
+                    vis[nrow][ncol] = 1;
+                    q.push({nrow,ncol});
+                }
+            
+            }
+            
             
         }
         for (int i = 0; i < rows; i++) {
